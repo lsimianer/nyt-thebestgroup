adeffc410a3313668a8b53dac5b5483948f68a9c
mcollums <collumsmichelle@gmail.com>
2019-06-10 14:11:42 -0500

    working on JS api url and added comments to html

--- index.js
+++ index.js
@@ -1,51 +1,35 @@
 // when search btn clicked > fx to trun that stores the following inputs into variables
 // inputs needed = search term, number of records, start year, end year
 
- $( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
 });
 
-var api = "https://www.omdbapi.com/?t=";
+var api = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=" + apiKey "&q=" + input + "&begin_date";
 // var TITLE = "the terminator";
-var apiKey = "&apikey=a5c61811";
+var apiKey = "rZvfCJAmSYG6Xug69Ga2vYrYsjsHo18G";
 
-var apiYearStart = $('#mtitle');
+var apiYearStart = $('#start-year' + '0101');
 
-var apiYearEnd = $('#mtitle');
+var apiYearEnd = $('#end-year' + '0101');
 
-var recordNum = $('#mtitle');
+var recordNum = $('#record-num');
 
-var input = $('#mtitle');
+var input = $('#search-input');
 
-$('#submit').on('click',articleSearch);
+$('#submit').on('click', articleSearch);
 
 console.log($("#submit"))
 
-
-function movieSearch() {
+function articleSearch() {
     event.preventDefault();
 
-var url = api + input.val() + apiKey;
-console.log(url);
-$.ajax({
-    url: url,
-    method: "GET"
-}).then(function(response) {
-    // create new row
-    var tRow = $("<tr>");
+    var url = api + input.val() + apiKey;
+    console.log(url);
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function (response) {
 
-    var titleTd = $("<td>").text(response.Title);
-    var yearTd = $("<td>").text(response.Year);
-    var actorsTd = $("<td>").text(response.Actors);
-    var ratingsTd = $("<td>").text(response.Ratings[0].Value);
-
-    tRow.append(titleTd, yearTd, actorsTd, ratingsTd);
-    
-    $("tbody").append(tRow);
-    console.log(response);
-    console.log(response.Ratings[0].Value);
-
-    // $('#m > tbody:last-child').append(tRow);
-
-});
+    });
 
