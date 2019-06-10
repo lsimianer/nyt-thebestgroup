49ea690b98ccf8c959c2ca31dedacf4bb15a6e55
lukas simianer <lsimianer@gmail.com>
2019-06-10 14:03:28 -0500

    js fx almsot done

--- index.js
+++ index.js
@@ -22,30 +22,28 @@ $('#submit').on('click',articleSearch);
 console.log($("#submit"))
 
 
-function movieSearch() {
+function articleSearch() {
     event.preventDefault();
 
-var url = api + input.val() + apiKey;
-console.log(url);
-$.ajax({
-    url: url,
-    method: "GET"
-}).then(function(response) {
-    // create new row
-    var tRow = $("<tr>");
+    var url = api + input.val() + apiKey;
+    console.log(url);
+    $.ajax({
+        url: url,
+        method: "GET"
+    }).then(function(response) {
+        // create new row
+        var article = $("<article>");
 
-    var titleTd = $("<td>").text(response.Title);
-    var yearTd = $("<td>").text(response.Year);
-    var actorsTd = $("<td>").text(response.Actors);
-    var ratingsTd = $("<td>").text(response.Ratings[0].Value);
+        var title = $("<h3>").text(response.Title);
+        var year = $("<h3>").text(response.Year);
+        // 
+        article.append(title, year, actors, ratings);
+        
+        $("search-results").append(article);
+        console.log(response);
+        console.log(response.Ratings[0].Value);
 
-    tRow.append(titleTd, yearTd, actorsTd, ratingsTd);
-    
-    $("tbody").append(tRow);
-    console.log(response);
-    console.log(response.Ratings[0].Value);
+        // $('#m > tbody:last-child').append(tRow);
 
-    // $('#m > tbody:last-child').append(tRow);
-
-});
-
+    });
+}
